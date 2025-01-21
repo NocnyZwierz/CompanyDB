f41458d04ab6fb06e35121b0d0e3069a51a770f7
Paweł <p.grabczak@cognitionhub.com>
2025-01-21 14:46:19 +0100

    Module 29.2

--- models/employees.model.js
+++ models/employees.model.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 
 const employeesSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  department: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Department' }
+  firstName: { type: String, required: true, minlength: 1 },
+  lastName: { type: String, required: true, minlength: 1 },
+  department: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Department' },
 });
-
 module.exports = mongoose.model("Employees", employeesSchema);
